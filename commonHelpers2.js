75ed41d47cfb0c4f41e4c88952187623059e0469
Volodymyr-Synhaievskyi <140277413+Volodymyr-Synhaievskyi@users.noreply.github.com>
2024-01-24 19:47:22 +0000

    Deploying to gh-pages from @ Volodymyr-Synhaievskyi/goit-advancedjs-hw@10fa67530ed5a58d64051fc0e0a7b29a472e83a8 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as m,t as a}from"./assets/vendor-023b031a.js";const t="videoplayer-current-time",n=document.querySelector("iframe"),o=new m(n),e=localStorage.getItem(t),c=a(r=>{localStorage.setItem(t,r.seconds)},1e3);e&&o.setCurrentTime(e);o.on("timeupdate",c);
+//# sourceMappingURL=commonHelpers2.js.map
